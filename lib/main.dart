dd9d323f8c3eaa9c3888cfa953975e9d8b60d40b
Leandre <leajapo@gmail.com>
2019-11-01 12:53:10 +0200

    feat:added provider package for state management, refactored screens to accomodate provider features

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import './screens/product_detail_screen.dart';
 import './screens/product_overview_screen.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        accentColor: Colors.amber,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      builder: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'Shop',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          accentColor: Colors.amber,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
     );
   }
 }
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
+  static const routeName = '/product-detail';
+
+  // final String title;
+  // ProductDetailScreen(this.title);
+
   @override
   Widget build(BuildContext context) {
+    final String productId =
+        ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context)
+        .items
+        .firstWhere((product) => product.id == productId);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(''),
+        title: Text(loadedProduct.title),
       ),
       body: Container(),
     );
