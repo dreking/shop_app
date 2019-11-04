689913e2eedb55b5f8bb551602166c91e6a1aed3
Leandre <leajapo@gmail.com>
2019-11-04 16:58:12 +0200

    feat:added a snackbar notification

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -45,6 +45,19 @@ class ProductItem extends StatelessWidget {
             icon: Icon(Icons.shopping_cart),
             onPressed: () {
               cart.addItem(product.id, product.title, product.price);
+              Scaffold.of(context).hideCurrentSnackBar();
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text(
+                  'Item added to cart',
+                ),
+                action: SnackBarAction(
+                  label: 'Undo',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+                duration: Duration(seconds: 2),
+              ));
             },
             color: Theme.of(context).accentColor,
           ),
