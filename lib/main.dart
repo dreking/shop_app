e16d79a354275e095a243af3d601d739d1feff97
Leandre <leajapo@gmail.com>
2019-11-04 17:27:29 +0200

    feat:added user product screen and now displaying every product

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/screens/user_products_screen.dart';
 
 import './providers/orders.dart';
 import './screens/cart_screen.dart';
@@ -41,6 +42,7 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
         },
       ),
     );
---
+++ lib/widgets/user_product_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class UserProductItem extends StatelessWidget {
+  final String title, imageUrl;
+
+  UserProductItem(this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+      ),
+      title: Text(title),
+      trailing: Container(
+        width: 100,
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.edit),
+              color: Theme.of(context).primaryColor,
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {},
+              color: Theme.of(context).errorColor,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
